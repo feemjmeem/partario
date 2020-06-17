abc4949c83921b11b04cd77847a2682b4a2e8a5a
Marc R. Uchniat <logan5@feem.net>
2020-06-16 19:13:26 -0500

    add getfile functionality to partarutil

--- partarutil.py
+++ partarutil.py
@@ -1,4 +1,6 @@
 import json
+import requests
+from os import path
 
 def loadconfig(part = None):
     with open("config.json", "r") as cfg_json:
@@ -10,3 +12,22 @@ def loadconfig(part = None):
                 print(f"POOH THAT'S NOT CONFIG.JSON YOU'RE EATING: {type(e).__name__} - {e}" )
         else:
             return(cfg)
+
+def getfile(url = None, dest = None, force = False):
+    if (url is None) or (dest is None):
+        raise ValueError("getfile: both url and dest are required")
+    else:
+        if not path.exists(dest) or force == True:
+            grabby = None
+            try:
+                print("Attempting download of %s..." % url)
+                grabby = requests.get(url, allow_redirects = True)
+            except:
+                raise ValueError("aw hell download of %s failed" % (url))
+            try:
+                print("Download of %s complete." % url)
+                open(dest, "wb").write(grabby.content)
+            except:
+                raise ValueError("aw hell i couldn't write %s" % (dest))
+        else:
+            raise ValueError("aw hell %s is already present (use force=True to override)" % dest)
