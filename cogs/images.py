e4ac1ce1d7eabd7f1b57b3d04fe2ecbd2e9517b3
Marc R. Uchniat <logan5@feem.net>
2020-06-16 19:19:02 -0500

    add getfonts command, install pillow

--- cogs/images.py
+++ cogs/images.py
@@ -1,13 +1,27 @@
 # teaching a bot to see
-from partarutil import loadconfig
+from partarutil import loadconfig,getfile
 from discord.ext import commands
 
 # load config
 cfg = loadconfig("images")
+corecfg = loadconfig("core")
 
 class ImageProcessor(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.command()
+    @commands.is_owner()
+    async def getfonts(self, ctx):
+                for font in cfg["fonts"]:
+                    try:
+                        url = cfg["fonts"][font]["url"]
+                        dest = "%s/%s" % (corecfg["filedir"], url.split("/")[-1])
+                        getfile(url, dest)
+                    except Exception as e:
+                        await ctx.send(f"Oh no: {type(e).__name__} - {e}")
+                    else:
+                        await ctx.send("%s downloaded successfully." % (font))
+                        
 def setup(bot):
     bot.add_cog(ImageProcessor(bot))
