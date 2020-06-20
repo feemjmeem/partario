f94244712b30ce667c0aa1809f8a235fb8c7e466
Marc R. Uchniat <logan5@feem.net>
2020-06-19 20:42:23 -0500

    add working wikipedia search

--- cogs/wikipedia.py
+++ cogs/wikipedia.py
@@ -1,3 +1,4 @@
+import wikipedia
 from discord.ext import commands
 from partarutil import loadconfig
 
@@ -6,6 +7,20 @@ cfg = loadconfig("wikipedia")
 class WikiProcessor(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        
+    @commands.command()
+    async def w(self, ctx, *, sq: str):
+        try:
+            s = wikipedia.search(sq, results=1)
+        except Exception:
+            await ctx.send("I didn't find anything.")
+        else:
+            try:
+                p = wikipedia.page(s)
+            except Exception:
+                await ctx.send("Hmm. Can you be more a little more specific?")
+            else:
+                await ctx.send(p.url)
         
 def setup(bot):
     bot.add_cog(WikiProcessor(bot))
